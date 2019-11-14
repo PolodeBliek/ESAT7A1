cc64327642480d81381597f88dd076f4f5ac8712
PolodeBliek <polodebliek@gmail.com>
2019-11-14 16:34:09 +0100

    Path Test

---
+++ code/Testing/PathTest.py
@@ -0,0 +1,21 @@
+import os
+import platform
+
+
+contents = ""
+if platform.system() == "Windows":
+    try:
+        currentDir = os.path.dirname(os.path.abspath(__file__)).replace("code\\Testing", "")
+        f = open(currentDir + "\\code\\Testing\\RandText.txt", "r")
+        contents = f.read()
+    except:
+        print("FAIL")
+else:
+    try:
+        currentDir = os.path.dirname(os.path.abspath(__file__)).replace("code//Testing", "")
+        f = open(currentDir + "//code//Testing//RandText.txt", "r")
+        contents = f.read()
+    except:
+        print("FAIL")
+if contents == "Alpha Bravo Charlie":
+    print("SUCCES")
--- code/Testing/Punten_vinden_plus_tekenen.py
+++ code/Testing/Punten_vinden_plus_tekenen.py
@@ -9,8 +9,10 @@ import matplotlib
  boven = coord[i][0]
  links = coord[i][1]
  rechts = coord[i][2]
- onder = coord[i][3] 
+ onder = coord[i][3]
 """
+
+print("There you go Olivia")
 
 def hoekpunten_vinden(matrix_anneloes):
 
@@ -23,7 +25,6 @@ def hoekpunten_vinden(matrix_anneloes):
         for rij in range(len(matrix_anneloes)):
             for kolom in range(len(matrix_anneloes[0])):
                 if matrix_anneloes[rij][kolom] == i:
-
                     if coord[i-1][0] == 0 or (coord[i-1][0])[0] > rij:
                         coord[i-1][0] = (rij,kolom)
 
@@ -35,7 +36,6 @@ def hoekpunten_vinden(matrix_anneloes):
 
                     if coord[i-1][3] == 0 or (coord[i-1][3])[0] < rij:
                         coord[i-1][3] = (rij, kolom)
-
     return coord
 
 
@@ -47,8 +47,8 @@ def distance_between_pixels_in_pixels(pixel1,pixel2):
 
     return direct_distance
 
-def pixel_length_to_real_length(pixellenght):
-    return ("%.1f" % (pixellenght/21.0)) #afronding van echte
+def pixel_length_to_real_length(pixellength):
+    return ("%.1f" % (pixellength/21.0)) #afronding van echte
 
 def midden_lijn(pixel1,pixel2):
     x_midden = (pixel1[0]+pixel2[0])/2
@@ -74,7 +74,7 @@ draw = ImageDraw.Draw(im)
 
 
 #matrix van annelies na bewerkingen
-matrix_anneloes = pickle.load(open("Dag_lieve_schat.pkl","rb"))
+matrix_anneloes = pickle.load(open(currentDir + "code\\Testing\\" + "Dag_lieve_schat.pkl","rb"))
 
 coord = hoekpunten_vinden(matrix_anneloes)
 
